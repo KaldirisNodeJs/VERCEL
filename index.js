73e2d056b9e856cb2d6878649b3663b82511a657
Kaldiris <kaldiris@gmail.com>
2023-05-18 18:29:24 -0300

    Ajustou Path CSS
    CSS

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--======== CSS ======== -->
-    <link rel="stylesheet" href="root.css">
+    <link rel="stylesheet" href="./root.css">
     <title>Greek In Vercel</title> 
   </head>
   <body>
