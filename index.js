39ab2f555597fbfa0d2977b5083be2313e2ecb4b
Kaldiris <kaldiris@gmail.com>
2023-05-18 14:00:43 -0300

    Form
    Form

--- index.js
+++ index.js
@@ -7,10 +7,10 @@
 const API_Filmes   = require("./filmes.cjs")
 const express      = require('express')
 const asyncHandler = require("express-async-handler");
+const url          = require('url');
 const fs           = require("fs");
 const ejs          = require("ejs");
 // const FormData = require("form-data");
-
 
 const app = express()
 const PORT = 4000
@@ -23,7 +23,10 @@ app.listen(PORT, () => {
 })
 
 app.get('/', (req, res) => {
-  res.send('API NO VERCEL FUNCIONANDO - By Grego')
+  const xurloriginal = req.protocol + '://' + req.get('host') + req.originalUrl;
+  let xurl = xurloriginal + 'filmes';
+  let xret = 'API NO VERCEL FUNCIONANDO - By Grego<br><a href="'+xurl+'" target="_blank">Abrir Rota Filmes</a>'
+  res.send(xret);
 })
 
 app.get('/about', (req, res) => {
@@ -34,20 +37,22 @@ app.get('/about', (req, res) => {
 app.get("/filmes",asyncHandler(async (req, res) => {
  
   await API_Filmes.getFilmePopular();   //.then((ret) => {});
-  res.send(global.filmePopular);
+  
+  //res.send(global.filmePopular);
 
-
-  // let file = "./filmes_rank.html";
-  // let xTemplate = fs.readFile(file, (err, buffer) => {
-  //   if (err) {
-  //     console.error("ERRO >>>>:", err);
-  //     return;
-  //   }
-  //   let html = buffer.toString();
-  //   let xhtml = ejs.render(html, { dados: global.filmePopular });
-  //   res.send(xhtml);
-  // });
-
+  let file = "./filmes_rank.html";
+  var xhtml;
+  var html;
+  
+  fs.readFile(file, (err, buffer) => {
+    if (err) {
+      console.error("ERRO >>>>:", err);
+      return;
+    }
+    var html = buffer.toString();
+    xhtml    = ejs.render(html, { dados: global.filmePopular });
+    res.send(xhtml);
+  });
 
 
 }));
